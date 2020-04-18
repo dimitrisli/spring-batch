e2a6310b811c635dc4d272f5934945e37b594beb
Dimitrios (Dimi) Liapis <dimitrisli@gmail.com>
2020-04-19 00:15:11 +0200

    Revert "BATCH-2480 Fix regression due to the fix of BATCH-2442"
    This reverts commit 1a0d78694fc3b1878914665d100abbf076eb069f.

--- spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProcessor.java
+++ spring-batch-core/src/main/java/org/springframework/batch/core/step/item/FaultTolerantChunkProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2019 the original author or authors.
+ * Copyright 2006-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -580,7 +580,8 @@ public class FaultTolerantChunkProcessor<I, O> extends SimpleChunkProcessor<I, O
 		Chunk<I>.ChunkIterator inputIterator = inputs.iterator();
 		Chunk<O>.ChunkIterator outputIterator = outputs.iterator();
 
-		if (!inputs.getSkips().isEmpty() && inputs.getItems().size() != outputs.getItems().size()) {
+		//BATCH-2442 : do not scan skipped items
+		if (!inputs.getSkips().isEmpty()) {
 			if (outputIterator.hasNext()) {
 				outputIterator.remove();
 				return;
